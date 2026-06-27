a62906f4327805c81c74f19c2fe9c174f5ab5f0f
Azhy-Slemany <azhysulemany@gmail.com>
2026-06-27 23:02:24 +0300

    fix: create more users, and assign roles to them

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -13,10 +12,39 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
+        // All users' email and passwords for clarification
+        // [admin@example.com, admin$$$]
+        // [support1@example.com, support1$$$], [support2@example.com, support2$$$], [support3@example.com, support3$$$]
+        // [customer1@example.com, customer1$$$], [customer2@example.com, customer2$$$], [customer3@example.com, customer3$$$]
+
+        $admin = User::createOrFirst([
             'name' => 'admin',
             'email' => 'admin@example.com',
-            'password' => 'admin123$$$',
+        ], [
+            'password' => 'admin$$$',
         ]);
+        $admin->assignRole('admin');
+
+        $supportList = ['support1', 'support2', 'support3'];
+        foreach ($supportList as $support) {
+            $support = User::createOrFirst([
+                'name' => $support,
+                'email' => "$support@example.com",
+            ], [
+                'password' => $support . '$$$',
+            ]);
+            $support->assignRole('support');
+        }
+
+        $customerList = ['customer1', 'customer2', 'customer3'];
+        foreach ($customerList as $customer) {
+            $customer = User::createOrFirst([
+                'name' => $customer,
+                'email' => "$customer@example.com",
+            ], [
+                'password' => $customer . '$$$',
+            ]);
+            $customer->assignRole('customer');
+        }
     }
 }
