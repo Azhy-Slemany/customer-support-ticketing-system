1c5e78d82adae40de5cecb65e45d6e1bbe68b6d3
Azhy-Slemany <azhysulemany@gmail.com>
2026-06-29 22:35:43 +0300

    feat: implement Ticket relationships

--- backend/app/Models/Ticket.php
+++ backend/app/Models/Ticket.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Ticket extends Model
 {
     protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(TicketComment::class);
+    }
 }
