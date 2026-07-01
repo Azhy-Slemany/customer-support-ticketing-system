fe0ce2ce5c87906a847c91289166bc4c631eb1df
Azhy-Slemany <azhysulemany@gmail.com>
2026-07-02 00:46:26 +0300

    fix: update relationships of the getTicket route

--- backend/app/Http/Controllers/Api/TicketController.php
+++ backend/app/Http/Controllers/Api/TicketController.php
@@ -32,7 +32,12 @@ class TicketController extends Controller
     }
 
     public function getTicket(Request $request, $id) {
-        $ticket = Ticket::with('customer')->find($id);
+        $ticket = Ticket::with([
+            'customer',
+            'comments',
+            'comments.user',
+            'comments.user.roles'
+        ])->find($id);
         if (!$ticket) {
             return $this->errorResponse('ticket not found', statusCode: 404);
         }
