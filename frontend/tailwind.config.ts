30ef4c091970a603b0cfe01bcb096a2a4615d9e6
Azhy-Slemany <azhysulemany@gmail.com>
2026-07-03 00:28:08 +0300

    fix: update Next.js configuration for standalone output and remove preflight reset in Tailwind CSS

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,9 +2,6 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-    corePlugins: {
-        preflight: false, // prevents Tailwind resetting Ant Design's base styles
-    },
     content: [
         './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
